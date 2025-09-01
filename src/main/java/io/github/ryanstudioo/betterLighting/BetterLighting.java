f6a22c06b8ea93aea3ec24fab6371318ec44453e
RyanStudioo <yanyan2019ry@gmail.com>
2025-09-02 06:14:03 +0800

    Ambient lighting with torches, lanterns, soultorches, and soul lanterns

--- src/main/java/io/github/ryanstudioo/betterLighting/BetterLighting.java
+++ src/main/java/io/github/ryanstudioo/betterLighting/BetterLighting.java
@@ -4,10 +4,6 @@ import org.bukkit.block.Block;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
 
 
 public final class BetterLighting extends JavaPlugin implements Listener {
